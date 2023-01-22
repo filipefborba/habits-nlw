402388f70e5ee01336583389f184d20585667a46
filipefborba <filipefborba@gmail.com>
2023-01-22 17:18:08 -0300

    finished web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -16,15 +16,16 @@ type Summary = {
   date: string;
   amount: number;
   completed: number;
-}[];
+};
 
 export function SummaryTable() {
-  const [summary, setSummary] = useState<Summary>([]);
+  const [summary, setSummary] = useState<Summary[]>([]);
 
   useEffect(() => {
-    api.get("summary").then((response) => {
-      setSummary(response.data);
-    });
+    // api.get("summary").then((response) => {
+    //   console.log(response.data);
+    //   setSummary(response.data);
+    // });
   }, []);
 
   return (
@@ -43,20 +44,21 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
-          const dayInSummary = summary.find((day) => {
-            return dayjs(date).isSame(day.date, "day");
-          });
+        {summary.length > 0 &&
+          summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) => {
+              return dayjs(date).isSame(day.date, "day");
+            });
 
-          return (
-            <HabitDay
-              key={date.toString()}
-              date={date}
-              amount={dayInSummary?.amount}
-              completed={dayInSummary?.completed}
-            />
-          );
-        })}
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            );
+          })}
 
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map((_, i) => {
